13051a9f0758eb79be7281759d439f6ae1d8dfe7
Naygel22 <piotrkozlowski14@gmail.com>
2023-11-29 21:17:06 +0100

    answers without text

--- giftsExchange.js
+++ giftsExchange.js
@@ -1,7 +1,44 @@
   const startButton = document.querySelector('.startButton');
   const startScreen = document.querySelector('.startScreen');
+  const answers = document.querySelector('.answers');
+  const inputBar = document.querySelector('.inputBar');
+ 
+//  inputBar.addEventListener('input', () => {
+//   const newAnswer = createNewAnswer(inputBar.value); 
+// 	answerBar.appendChild(newAnswer);
+//   inputBar.value = ''; 
+//   });
 
-  startButton.addEventListener('click', () => {
-    startScreen.style.display = 'none';
+  function handleKeyPress(event) {
+    if(event.keyCode === 13){
+      event.preventDefault();
+      const newAnswer = createNames(inputBar.value); 
+	  answers.appendChild(newAnswer);
+    inputBar.value = ''; 
+    } 
+  }
+  inputBar.addEventListener('keydown', handleKeyPress);
+ 
+ 
+  function createNames(text){
+  const newName = document.createElement('div');
+  newName.className = 'answerItem';
+
+  const newNameText = document.createElement('div');
+  newNameText.className = 'answerText';
+  newNameText.appendChild(document.createTextNode(text));
+  
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'deleteButton';
+  deleteButton.appendChild(document.createTextNode("X"));
+  deleteButton.addEventListener('click', () => {
+    newName.remove(); 
   });
+
+  newName.appendChild(newNameText);
+  newName.appendChild(deleteButton);
+
+  return newName;
+
+ }
 
